affc79efe7008f80629b588e8666cf29455fe2b4
AlexRykov1 <rykov818@gmail.com>
2023-02-24 14:13:32 +0300

    edit dateTo substrings in ItemAdapter.kt (46 row) to Red color text Alert.

--- app/src/main/java/com/example/bc_parking1beta/ItemAdapter.kt
+++ app/src/main/java/com/example/bc_parking1beta/ItemAdapter.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.icu.util.Calendar
 import android.icu.util.LocaleData
 import android.os.Build
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
@@ -19,13 +20,13 @@ import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-class ItemAdapter: ListAdapter<ParkItem, ItemViewHolder>(ParkItemDiffCallback()){
+class ItemAdapter : ListAdapter<ParkItem, ItemViewHolder>(ParkItemDiffCallback()) {
 
     var onParkItemLongClickListener: ((ParkItem) -> Unit)? = null
     var onParkItemClickListener: ((ParkItem) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val layout = when(viewType){
+        val layout = when (viewType) {
             VIEW_TYPE_ENABLED -> R.layout.item_park
             VIEW_TYPE_DISABLED -> R.layout.item_park_disabled
             else -> throw RuntimeException("Unknown View type $viewType")
@@ -46,12 +47,20 @@ class ItemAdapter: ListAdapter<ParkItem, ItemViewHolder>(ParkItemDiffCallback())
             val time = Calendar.getInstance().time
             val formatter = SimpleDateFormat("dd.M.yyyy")
             val current = formatter.format(time)
-            return if (parkItem.dateTo.length > 5){
-                val year = current.substring(4,8)
-                val year1 = parkItem.dateTo.substring(4,8)
-                val month = current.substring(2,3)
-                val month1 = parkItem.dateTo.substring(2,3)
-                viewHolder.tv_name.text = "$year $year1 $month $month1"
+            return if (parkItem.dateTo.length > 5) {
+                val year = current.substring(5, 9)
+                val year1 = when (parkItem.dateTo.length) {
+                    8 -> parkItem.dateTo.substring(4, 8)
+                    9 -> parkItem.dateTo.substring(5, 9)
+                    else -> parkItem.dateTo.substring(6, 10)
+                }
+                val month = current.substring(3, 4)
+                val month1 = when (parkItem.dateTo.length) {
+                    8 -> parkItem.dateTo.substring(2, 3)
+                    9 -> parkItem.dateTo.substring(3, 4)
+                    else -> parkItem.dateTo.substring(4, 5)
+                }
+                Log.d("a1", "$year $year1 $month $month1")
                 (year == year1 && month == month1)
             } else {
                 false
@@ -67,8 +76,8 @@ class ItemAdapter: ListAdapter<ParkItem, ItemViewHolder>(ParkItemDiffCallback())
             onParkItemClickListener?.invoke(parkItem)
         }
 //        IT'S     OUTSIDE     ItemPark    VIEW     !  !  !
-        viewHolder.tv_name.text =parkItem.name
-        viewHolder.tv_date_to.text =parkItem.dateTo
+        viewHolder.tv_name.text = parkItem.name
+        viewHolder.tv_date_to.text = parkItem.dateTo
         viewHolder.tvCount.text = parkItem.id.toString()
 //        parkItem.enabled = getItemViewType(position) == VIEW_TYPE_ENABLED
 //        if (parkItem.enabled) {
@@ -87,9 +96,9 @@ class ItemAdapter: ListAdapter<ParkItem, ItemViewHolder>(ParkItemDiffCallback())
 
     override fun getItemViewType(position: Int): Int {
         val item = getItem(position)
-        return if (item.dateTo == "clear"){
+        return if (item.dateTo == "clear") {
             VIEW_TYPE_ENABLED
-        } else{
+        } else {
             VIEW_TYPE_DISABLED
         }
     }
